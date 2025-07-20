8327c38f1b714f72059aa179b955c683243aed6d
Samadhan <samadhankadam002@gmail.com>
2025-07-20 13:00:40 +0000

    add sign in with google and phone

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import SignUpForm from '@/components/auth/sign-up-form';
 
 export default function SignUpPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex min-h-screen items-center justify-center bg-background p-4">
       <SignUpForm />
     </div>
   );
